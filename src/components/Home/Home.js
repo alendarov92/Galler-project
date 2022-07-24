45b2c73dbfbd429bc755f3b03da345783b183fed
alendarov92 <104994198+alendarov92@users.noreply.github.com>
2022-07-24 10:21:35 +0300

    Render latest game conndisionali

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,28 +8,30 @@ const Home = () => {
 
     useEffect(() => {
         gallerServices.getAll()
-        .then(result => {
-            setGallery(result)
-        }, [])
+            .then(result => {
+                setGallery(result)
+            }, [])
     });
-    return(
+    return (
         <section id="welcome-world">
-                <div className="welcome-message">
-                    <h2>You Can Find</h2>
-                    <h3>THE NATURE Here</h3>
-                </div>
-                <img
-                    src="https://i.pinimg.com/736x/72/16/a7/7216a77ec1ae893b9b38b3b596f69392.jpg"
-                    alt="hero"
-                />
-                <div id="home-page">
-                    <h1>Latest Pictures</h1>
-                    {/* Display div: with information about every game (if any) */}
-                    {gallery.map(x => <LatestCard card={x} />)}
-                    {/* Display paragraph: If there is no games  */}
-                    <p className="no-articles">No articles yet</p>
-                </div>
-            </section>
+            <div className="welcome-message">
+                <h2>You Can Find</h2>
+                <h3>THE NATURE Here</h3>
+            </div>
+            <img
+                src="https://i.pinimg.com/736x/72/16/a7/7216a77ec1ae893b9b38b3b596f69392.jpg"
+                alt="hero"
+            />
+            <div id="home-page">
+                <h1>Latest Pictures</h1>
+
+                {gallery.length > 0
+                    ? gallery.map(x => <LatestCard key={x._id} card={x} />)
+                    : <p className="no-articles">No articles yet</p>
+                }
+
+            </div>
+        </section>
     )
 }
 
