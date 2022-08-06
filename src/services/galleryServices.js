ac6861ce1c0b0382973973507f6150060eab6d09
alendarov92 <104994198+alendarov92@users.noreply.github.com>
2022-08-06 08:59:40 +0100

    style corection

--- src/services/galleryServices.js
+++ src/services/galleryServices.js
@@ -1,6 +1,6 @@
-const baseUrl = 'http://localhost:3030';
+const baseUrl = 'http://localhost:3030/jsonstore/gallery';
 
 export const getAll = () => {
-    return fetch(`${baseUrl}/jsonstore/gallery`)
+    return fetch(`${baseUrl}`)
         .then(res => res.json())
 }
